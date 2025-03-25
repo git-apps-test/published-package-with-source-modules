ee406f9bc54eba09bf082e6b0768bfb55c739eb0
manish kumar <manish@appsmith.com>
2025-03-25 14:57:34 +0530

    change set 2 for feature-3

--- modules/JSModule1/jsobjects/JSModule1/JSModule1.js
+++ modules/JSModule1/jsobjects/JSModule1/JSModule1.js
@@ -8,6 +8,7 @@ export default {
 	async myFun2 () {
 		// some newer comments before commit to another branch;
 		// added comments for feature-3 and what not;
+		// comment 2 for feature-3
 		return "version 1.2.3";
 		//	use async-await or promises
 		//	await storeValue('varName', 'hello world')
