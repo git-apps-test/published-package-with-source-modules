c5a3cf56e50ed94991baaf748784d6caa7a5ad64
manish kumar <manish@appsmith.com>
2025-03-25 14:54:06 +0530

    some changes

--- modules/JSModule1/jsobjects/JSModule1/JSModule1.js
+++ modules/JSModule1/jsobjects/JSModule1/JSModule1.js
@@ -6,6 +6,7 @@ export default {
 		return QueryModule1.data;
 	},
 	async myFun2 () {
+		// some newer comments before commit to another branch;
 		return "version 1.2.3";
 		//	use async-await or promises
 		//	await storeValue('varName', 'hello world')
